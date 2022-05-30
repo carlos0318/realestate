8a90372594650580319ecfdd278f060f46583943
Carlos Ortega <carlos.ortega.ast0318@gmail.com>
2022-05-30 18:11:38 -0500

    fix: fetchApi

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       "X-RapidAPI-Host": "bayut.p.rapidapi.com",
-      "X-RapidAPI-Key": "6206c2d5d3msh4b6ad71f44829f8p177201jsn926ae3158671",
+      "X-RapidAPI-Key": process.env.API,
     },
   });
 
